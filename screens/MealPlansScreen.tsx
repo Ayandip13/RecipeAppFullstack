0d80835d4b5ff6fdf898d3dc32d4500647456d1c
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-06-14 12:29:11 +0530

    implement navigation structure with bottom tabs and screens

---
+++ screens/MealPlansScreen.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const MealPlansScreen = () => {
+  return (
+    <View>
+      <Text>MealPlansScreen</Text>
+    </View>
+  );
+};
+
+export default MealPlansScreen;
